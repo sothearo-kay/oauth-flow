89ca42e8e407e092e326af7a2068151a4a55dcc2
sothearo-kay <sothearo.kay@techostartup.center>
2025-06-06 09:38:42 +0700

    refactor: remove unused block

--- src/stores/counter.ts
+++
@@ -1,9 +0,0 @@
-export const useCounterStore = defineStore("counter", () => {
-  const count = ref(0);
-
-  const increment = () => {
-    count.value++;
-  };
-
-  return { count, increment };
-});
